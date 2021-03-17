70c018ce2ddd6f315286c566e7042b5ae3437bea
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-03-18 00:22:04 +0530

    add user to room when enabled

--- app/outOfOffice/server/index.ts
+++ app/outOfOffice/server/index.ts
@@ -2,12 +2,14 @@ import { Meteor } from "meteor/meteor";
 
 import { Users, Rooms } from "../../models/server";
 import { executeSendMessage } from "../../lib/server/methods/sendMessage";
+import { addUserToRoom } from "../../lib/server/functions/addUserToRoom";
 
 import { IUser } from "../../../definition/IUser";
 import { IRoom } from "../../../definition/IRoom";
 
 const kameUserId = "JMXwCKiQ9v28Hf7Ef";
 const camelRidersRoomId = "mPcbxJxRzzjwdCF7f";
+const deputyUserId = "8hWHiTdK7yTEsXCxo";
 
 Meteor.methods({
   enableUserOutOfOffice() {
@@ -19,15 +21,18 @@ Meteor.methods({
 
     const myUser: IUser = Users.findOneById(kameUserId);
     const myRoom: IRoom = Rooms.findOneById(camelRidersRoomId);
+    const myDeputy: IUser = Users.findOneById(deputyUserId);
 
     const myMessage = {
-      msg: "This user is **out of office**",
+      msg: "I am now **out of office**. __My deputy will be added to the room.__",
       rid: myRoom._id,
     };
 
+    // send a message to channel
     executeSendMessage(myUser._id, myMessage);
 
-    // TODO: add deputy to room
+    // add a deputy to the room
+    addUserToRoom(myRoom._id, myDeputy, myUser, false);
   },
   disableUserOutOfOffice() {
     if (!this.userId) {
